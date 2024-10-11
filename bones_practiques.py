d7070a98de49bda4b4308316f86d2e078dcac5d0
aroca <aroca@instituticaria.cat>
2024-10-11 12:53:11 +0200

    enviament

--- bones_practiques.py
+++ bones_practiques.py
@@ -1,18 +1,22 @@
-"""
-Programa per demanar valors enters per al divisor i el dividend, i calcular el quocient i el residu de la divisió.
-Autor: [Arnau Roca Silva]
-Data: [2024/10/10]
-"""
+'''bones_practiques. Demana
+Institut Icària
+Programació - 1r Batxillerat - Curs 2023-24
+Descripció llarga del programa. Indicar que fa el
+programa i el resultat esperat.
+'''
+__author__ = "Arnau Roca Silva"
+__email__ = "aroca@instituticaria.cat"
+__date__ = "2024/10/10"
 # Demanem valors enters per a divisor i dividend:
 
-dividend  =int(input("Introdueix el dividend: "))
-divisor=int(input("Introdueix el divisor: "))
+dividend = int(input("Introdueix el dividend: "))
+divisor = int(input("Introdueix el divisor: "))
 
-#Calculem el residu i el quocient:
-residu = dividend%divisor
+# Calculem el residu i el quocient:
+residu = dividend % divisor
 quocient = dividend//divisor
 
-#Mostrem per pantalla de manera ordenada:
+# Mostrem per pantalla de manera ordenada:
 print(f"Divisió: {dividend}/{divisor}")
 print(f"Quocient: {quocient}")
 print(f"Residu: {residu}")
